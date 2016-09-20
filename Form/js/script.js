f12ff9f94d9f4bab5a6f39860c31c2425ad2b4a1
Yuliya Ryadskaya <juliya.vasilisa@gmail.com>
2016-09-20 20:52:18 +0300

    First commit 20.09.16

--- Form/js/script.js
+++ Form/js/script.js
@@ -1,1 +1,17 @@
+$('.textfield').hover(
+    function() {
+          $(this).next('.tipstext').stop().fadeIn();
+    },
+    function() {
+        $('.tipstext').stop().fadeOut();
+    });
+
+$(function() {
+    var $tipsCollection;
+       $('input[type="submit"]').on('click', function(e){
+       $tipsCollection = $('.tipstext');
+       $tipsCollection.css('display','inline-block');
+       e.preventDefault();
+    });
+});
 
