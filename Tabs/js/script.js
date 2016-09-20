790e0a17bc68835a447615012ecff840a2e69843
Yuliya Ryadskaya <juliya.vasilisa@gmail.com>
2016-09-20 20:53:48 +0300

    First commit 20.09.16

--- Tabs/js/script.js
+++ Tabs/js/script.js
@@ -4,16 +4,10 @@ $(document).ready(function() {
         if(i != 0) $(this).hide(0);
     });
   tabs.on('click', '.tabs a', function(e){
-        /* Предотвращаем стандартную обработку клика по ссылке */
         e.preventDefault();
-/* Узнаем значения ID блока, который нужно отобразить */
         var tabId = $(this).attr('href');
-
-        /* Удаляем все классы у якорей и ставим active текущей вкладке */
         $('.tabs a',tabs).removeClass();
         $(this).addClass('active');
-
-        /* Прячем содержимое всех вкладок и отображаем содержимое нажатой */
         $('.tabs-content > div', tabs).hide(0);
         $(tabId).show();
     });
